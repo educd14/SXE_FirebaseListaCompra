dc8218ceaf2b2ac32b0d2203914a0e1cfc124c17
ecollazodominguez <ecollazodominguez@danielcastelao.org>
2020-03-16 20:17:16 +0100

    Firebase init + config

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AngularFireModule } from '@angular/fire';
+
+import { AppComponent } from './app.component';
+import { ListacompraComponent } from './listacompra/listacompra.component';
+import {environment} from '../environments/environment';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListacompraComponent
+  ],
+  imports: [
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyCCZDnqU9UdgIxaD7_kndeylvT72NsFGKM',
+    authDomain: 'lista-62bf6.firebaseapp.com',
+    databaseURL: 'https://lista-62bf6.firebaseio.com',
+    projectId: 'lista-62bf6',
+    storageBucket: 'lista-62bf6.appspot.com',
+    messagingSenderId: '964531457827',
+    appId: '1:964531457827:web:821307f92b58455592c696'
+
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
